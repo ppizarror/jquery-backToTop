8771e0e382d99e68e385abca0acfe1ad9785c794
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-06-17 05:43:28 +0100

    Added applyTheme function

--- jquery-backToTop.js
+++ jquery-backToTop.js
@@ -22,8 +22,8 @@
     /**
      * BackToTop class
      *
-     * @param {object} options - Initial creation options
      * @class
+     * @param {object} options - Initial creation options
      * @private
      */
     let _BackToTop = function (options) {
@@ -34,21 +34,21 @@
          * @since 0.0.1
          */
         let _defaults = {
-            autoOpen: true,         // Open back-to-top when created
-            bottom: 20,             // Bottom fixed position (px)
-            container: $('body'),   // Container of the object
-            effect: 'none',         // Effect of the button
-            height: 35,             // Height of the button (px)
-            right: 20,              // Right fixed position (px)
-            theme: 'default',       // Theme of the button
-            width: 35,              // Width of the button (px)
-            zIndex: 999,            // z-Index of the div
+            autoOpen: true,             // Open back-to-top when created
+            bottom: 20,                 // Bottom fixed position (px)
+            container: $('body'),       // Container of the object
+            effect: 'none',             // Effect of the button
+            height: 35,                 // Height of the button (px)
+            right: 20,                  // Right fixed position (px)
+            theme: 'default',           // Theme of the button
+            width: 35,                  // Width of the button (px)
+            zIndex: 999,                // z-Index of the div
         };
         this._options = $.extend(_defaults, options);
 
         /**
          * back-to-top main div
-         * @type {jQuery | HTMLElement | null}
+         * @type {jQuery | HTMLElement | JQuery | null}
          * @private
          * @since 0.0.1
          */
@@ -63,6 +63,22 @@
         this._opened = false;
 
         /**
+         * Actual button theme
+         * @type {string}
+         * @private
+         * @since 0.0.3
+         */
+        this._actualTheme = '';
+
+        /**
+         * Actual effect theme
+         * @type {string}
+         * @private
+         * @since 0.0.3
+         */
+        this._actualEffect = '';
+
+        /**
          * Saves pointer to object
          * @type {_BackToTop}
          * @private
@@ -71,7 +87,40 @@
         let self = this;
 
         /**
-         * Append back-to-top div if not created
+         * Apply a theme to the button
+         * @function
+         * @param {string} theme - Theme of the button
+         * @private
+         * @since 0.0.3
+         */
+        this._applyTheme = function (theme) {
+
+            /**
+             * If button is has not been built returns
+             */
+            if (this._obj === null) return;
+
+            /**
+             * If previous theme has been applied
+             */
+            if (this._actualTheme !== '') {
+                this._obj.removeClass(this._actualTheme);
+            }
+
+            /**
+             * Generates CSS theme name
+             */
+            this._actualTheme = 'jquery-back-to-top-' + theme + '-default';
+
+            /**
+             * Apply theme class
+             */
+            this._obj.addClass(this._actualTheme);
+
+        };
+
+        /**
+         * Creates DOM object
          */
         // noinspection JSJQueryEfficiency
         if ($('#back-to-top-container').length === 0) {
@@ -80,11 +129,13 @@
              * Creates object
              */
             $(this._options.container).append('<div id="back-to-top-container" class="jquery-back-to-top"></div>');
-            this._obj = $('#back-to-top-container');
+            self._obj = $('#back-to-top-container');
 
             /**
              * Apply theme and effect
              */
+            this._applyTheme(this._options.theme);
+
         }
 
     };
