6a954b2a4be88cb657cd3f99a80bab14d3ecc413
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-06-17 05:27:44 +0100

    Version 0.0.2

--- create-dist.py
+++ create-dist.py
@@ -6,15 +6,9 @@ Generates compiled files to dist/
 @author Pablo Pizarro @ppizarror.com
 """
 
-# Main files
-CSS_FILE = 'dist/jquery-backToTop.min.css'
-JS_FILE = 'dist/jquery-backToTop.min.js'
-
-# Project files to add
-CSS = ['main.min.css', 'themes.min.css', 'effects.min.css']
-JS = ['jquery-backToTop.min.js']
-
-# Creates a file
+"""
+Genenerates a compiled file
+"""
 def generate_file(main, files):
     w = open(main, 'w')
     for i in files:
@@ -24,6 +18,14 @@ def generate_file(main, files):
         f.close()
     w.close()
 
-# Generate files
+# Main files
+CSS_FILE = 'dist/jquery-backToTop.min.css'
+JS_FILE = 'dist/jquery-backToTop.min.js'
+
+# Project files to add
+CSS = ['main.min.css', 'themes.min.css', 'effects.min.css']
+JS = ['jquery-backToTop.min.js']
+
+# Generate compiled files
 generate_file(CSS_FILE, CSS)
 generate_file(JS_FILE, JS)
--- jquery-backToTop.js
+++ jquery-backToTop.js
@@ -30,23 +30,70 @@
 
         /**
          * Default options
+         * @private
          * @since 0.0.1
          */
-        let defaults = {
-            bottom: 20,     // Bottom fixed position (px)
-            height: 35,     // Height of the button (px)
-            right: 20,      // Right fixed position (px)
-            width: 35,      // Width of the button (px)
-            zIndex: 999,    // z-Index of the div container
+        let _defaults = {
+            autoOpen: true,         // Open back-to-top when created
+            bottom: 20,             // Bottom fixed position (px)
+            container: $('body'),   // Container of the object
+            effect: 'none',         // Effect of the button
+            height: 35,             // Height of the button (px)
+            right: 20,              // Right fixed position (px)
+            theme: 'default',       // Theme of the button
+            width: 35,              // Width of the button (px)
+            zIndex: 999,            // z-Index of the div
         };
+        this._options = $.extend(_defaults, options);
+
+        /**
+         * back-to-top main div
+         * @type {jQuery | HTMLElement | null}
+         * @private
+         * @since 0.0.1
+         */
+        this._obj = null;
+
+        /**
+         * Indicates the current status of the back-to-top
+         * @type {boolean}
+         * @private
+         * @since 0.0.1
+         */
+        this._opened = false;
+
+        /**
+         * Saves pointer to object
+         * @type {_BackToTop}
+         * @private
+         * @since 0.0.1
+         */
+        let self = this;
+
+        /**
+         * Append back-to-top div if not created
+         */
+        // noinspection JSJQueryEfficiency
+        if ($('#back-to-top-container').length === 0) {
+
+            /**
+             * Creates object
+             */
+            $(this._options.container).append('<div id="back-to-top-container" class="jquery-back-to-top"></div>');
+            this._obj = $('#back-to-top-container');
+
+            /**
+             * Apply theme and effect
+             */
+        }
 
     };
 
     /**
      * Create jQuery plugin
      */
-    $.fn.backToTop = function (options) {
+    $.backToTop = function (options) {
         return new _BackToTop(options);
-    }
+    };
 
 }));
