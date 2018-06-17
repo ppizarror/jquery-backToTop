3c8bd074647edbf17998c5780c5098a04211371e
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-06-17 05:04:02 +0100

    Created main files

--- jquery-backToTop.js
+++ jquery-backToTop.js
@@ -3,10 +3,11 @@
  *
  * @licence MIT
  * @author Pablo Pizarro @ppizarror.com
+ * @version 0.0.1
  */
 
 ;(function (factory) {
-    /* global define, define.amd, jQuery */
+    /* global define, define.amd */
     /** @namespace define.amd */
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module
@@ -17,4 +18,35 @@
     }
 }(function ($) {
     "use scrict";
+
+    /**
+     * BackToTop class
+     *
+     * @param {object} options - Initial creation options
+     * @class
+     * @private
+     */
+    let _BackToTop = function (options) {
+
+        /**
+         * Default options
+         * @since 0.0.1
+         */
+        let defaults = {
+            bottom: 20,     // Bottom fixed position (px)
+            height: 35,     // Height of the button (px)
+            right: 20,      // Right fixed position (px)
+            width: 35,      // Width of the button (px)
+            zIndex: 999,    // z-Index of the div container
+        };
+
+    };
+
+    /**
+     * Create jQuery plugin
+     */
+    $.fn.backToTop = function (options) {
+        return new _BackToTop(options);
+    }
+
 }));
